9119feb8337255025135ca73bf13dc29b9262382
Amarbirsingh Kuldipsingh Randhawa <amarbirsingh.randhawa062019@gmail.com>
2024-12-27 22:13:23 +0530

    Added modules of CrewManagement, Log, and TravelPreferences

---
+++ src/main/java/com/airTransport/atm_backend/service/CrewManagement.java
@@ -0,0 +1,7 @@
+package com.airTransport.atm_backend.service;
+
+public interface CrewManagement {
+    public List<String> pilot();
+    public List<String> cabinCrew();
+    public List<String> groundStaff();
+}
