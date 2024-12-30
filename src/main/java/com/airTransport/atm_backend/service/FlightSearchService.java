e8ac7dfd951072e0049d233b3c7786b02d8ef500
aayush-19y <2021bcs103@sggs.ac.in>
2024-12-30 18:03:35 +0530

    Added Hot Reload

--- src/main/java/com/airTransport/atm_backend/service/FlightSearchService.java
+++ src/main/java/com/airTransport/atm_backend/service/FlightSearchService.java
@@ -3,7 +3,7 @@ package com.airTransport.atm_backend.service;
 import com.airTransport.atm_backend.model.Flight;
 import java.util.List;
 
-public interface FlightSearch {
+public interface FlightSearchService {
     List<Flight> sortByPrice();
     List<Flight> sortByAirline();
     List<Flight> sortByClass();
