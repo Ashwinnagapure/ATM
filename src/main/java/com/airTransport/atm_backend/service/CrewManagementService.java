58000e7dff5de0a0bfd5f824e719e3c01b89dea3
Amarbirsingh Kuldipsingh Randhawa <amarbirsingh.randhawa062019@gmail.com>
2024-12-30 11:06:05 +0530

    Chnages in CrewManagement Module

--- src/main/java/com/airTransport/atm_backend/service/CrewManagementService.java
+++ src/main/java/com/airTransport/atm_backend/service/CrewManagementService.java
@@ -2,50 +2,22 @@ package com.airTransport.atm_backend.service;
 
 import com.airTransport.atm_backend.model.CrewManagement;
 import com.airTransport.atm_backend.model.enums.Role;
-import com.airTransport.atm_backend.repository.CrewManagementRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class CrewManagementService {
-    @Autowired
-    private CrewManagementRepository crewMemberRepository;
+public interface CrewManagementService {
 
+    CrewManagement addCrewMember(CrewManagement crewMember);
 
-    public CrewManagement addCrewMember(CrewManagement crewMember) {
-        return crewMemberRepository.save(crewMember);
-    }
+    CrewManagement updateCrewMember(Long id, CrewManagement crewMember);
 
+    void deleteCrewMember(Long id);
 
-    public CrewManagement updateCrewMember(Long id, CrewManagement crewMember) {
-        CrewManagement existingCrew = crewMemberRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Crew Member not found"));
-        existingCrew.setName(crewMember.getName());
-        existingCrew.setRole(crewMember.getRole());
-        existingCrew.setAvailability(crewMember.isAvailability());
-        return crewMemberRepository.save(existingCrew);
-    }
+    CrewManagement getCrewMemberById(Long id);
 
+    List<CrewManagement> getCrewMembersByRole(Role role);
 
-    public void deleteCrewMember(Long id) {
-        crewMemberRepository.deleteById(id);
-    }
-
-
-    public CrewManagement getCrewMemberById(Long id) {
-        return crewMemberRepository.findById(id).orElseThrow(
-                () -> new RuntimeException("Crew Member not found"));
-    }
-
-
-    public List<CrewManagement> getCrewMembersByRole(Role role) {
-        return crewMemberRepository.findByRole(role);
-    }
-
-
-    public List<CrewManagement> getAvailableCrewMembers() {
-        return crewMemberRepository.findByAvailability(true);
-    }
+    List<CrewManagement> getAvailableCrewMembers();
 }
