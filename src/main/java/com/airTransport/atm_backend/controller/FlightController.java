53b3d183634c4a381834a2bb0fc618d683e42412
aayush-19y <2021bcs103@sggs.ac.in>
2024-12-31 15:59:14 +0530

    flight search and service segregated

--- src/main/java/com/airTransport/atm_backend/controller/FlightController.java
+++ src/main/java/com/airTransport/atm_backend/controller/FlightController.java
@@ -20,7 +20,7 @@ public class FlightController {
 
     @GetMapping("/test")
     public String test(){
-        return "Amar";
+        return "Testing Flight Controller";
     }
 
     @PostMapping("/schedule")
